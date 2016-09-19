c6b43de7656dbee9f785eee3cafc895967bddf90
Titus <blahblah>
2016-09-18 21:09:37 -0400

    modified response data format via JSON.parse method

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 
 //could use var (variable) here instead of const (constant), but if constant can be used (i.e., the variable never changes), using const is apparently more efficient.
 const https = require('https');
-var username = "yourUsername";
+var username = "yourTreehouseUsername";
 
 function printMessage(username, badgeCount, points) {
 	var message = username + " has " + badgeCount + " total badges and " + points + " JavaScript points ";
@@ -12,27 +12,46 @@ function printMessage(username, badgeCount, points) {
 
 
 //connect to API URL (https://teamtreehouse.com/username.json)
-
 var request = https.get("https://teamtreehouse.com/" + username + ".json", function (response) {
-	console.log(response.statusCode);
+	var body = "";
+	
 	//Read data from response
+	response.on('data', function (chunk) {
+		body += chunk;
+	});
+	response.on('end', function () {
+		var profile = JSON.parse(body);
+		printMessage(username, profile.badges.length, profile.points.JavaScript)
+		console.dir(profile);
+	});
+	
 	//Parse data
 	//Print data
 });
 
-//const https = require('https');
+
+
+
+////connect to API URL (https://teamtreehouse.com/username.json)
+//var request = https.get("https://teamtreehouse.com/" + username + ".json", (response) => {
+//  console.log('TESTstatusCode:', response.statusCode);
+//  console.log('TESTheaders:', response.headers);
+//	
+////Read data from response
 //
-//https.get('https://encrypted.google.com/', (res) => {
-//  console.log('statusCode:', res.statusCode);
-//  console.log('headers:', res.headers);
-//
-//  res.on('data', (d) => {
-//    process.stdout.write(d);
+//  response.on('data', (callmewhatever) => {
+//	//stdout = 'standard output'. process.stdout.write outputs user data associated with treehouse profile for particular user specified in var username. process.stdout.write similar to console.log, but in this case, console.log only returns a long list of numerical buffer values instead of a long string  of user data.
+//    process.stdout.write(callmewhatever);
+//	printMessage(username);
 //  });
 //
-//}).on('error', (e) => {
-//  console.error(e);
+//}).on('error', (errorVariable) => {
+//  console.error(errorVariable);
 //});
+//
+//
+////	//Parse data
+////	//Print data
 
 
 
